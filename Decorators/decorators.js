e264ed17dd98b95e31103194d38dcbd73f5c6439
João Paulo <joaopaulo_seixas@outlook.com>
2022-05-15 23:33:11 -0300

    learning about TS Decorators

---
+++ Decorators/decorators.js
@@ -0,0 +1,60 @@
+"use strict";
+// Desafio Decorator perfilAdmin
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const usuarioLogado = {
+    nome: "Guilherme Filho",
+    email: "guigui@gmail.com",
+    admin: false,
+};
+class MudancaAdministrativa {
+    critico() {
+        console.log("Algo critico foi alterado!");
+    }
+}
+new MudancaAdministrativa().critico();
+function perfilAdmin(constructor) {
+    return class extends constructor {
+        constructor(...args) {
+            super(...args);
+            if (!usuarioLogado || !usuarioLogado.admin) {
+                throw new Error("Acess Denied.");
+            }
+        }
+    };
+}
+class ContaCorrente {
+    constructor(saldo) {
+        this.saldo = saldo;
+    }
+    sacar(valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+        }
+        else {
+            return false;
+        }
+    }
+    getSaldo() {
+        return this.saldo;
+    }
+}
+__decorate([
+    congelar
+], ContaCorrente.prototype, "sacar", null);
+__decorate([
+    congelar
+], ContaCorrente.prototype, "getSaldo", null);
+const cc = new ContaCorrente(2000);
+cc.sacar(1800);
+console.log(cc.getSaldo());
+function congelar(alvo, nomePropriedade, descritor) {
+    console.log(alvo);
+    console.log(nomePropriedade);
+    descritor.writable = false;
+}
+//# sourceMappingURL=decorators.js.map
